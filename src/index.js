619d7594f01850daef27175efd60cee62395694a
Shodipo Ayomide <20538832+Developerayo@users.noreply.github.com>
2019-11-06 06:02:29 +0400

    make it align

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "./styles.css";
 function App() {
 	return (
 		<div className="App">
-			<h1>Hello Developer 🔥🔥🔥🔥</h1>
+			<h1>Hello Developer 🔥🔥🔥</h1>
 			<h2>Start editing to see some magic happen!😄🔥</h2>
 			<h3>Hehehe! sorry, you can't 😅😎🔥🔥</h3>
 
